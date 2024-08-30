31457286b04887c6a4b32c3cd0f03de75e7e4160
Shrikant Pawar <shrikantpawar703@gmail.com>
2024-08-30 00:35:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+using Constructor;
+
+public class Program
+{
+	public static void Main()
+	{
+      Student student = new Student();
+
+		Console.WriteLine("ParameterLess Constructor");
+		
+		// ParameterLess Constructor
+		Student student1 = new Student();
+		student1.ShowData();
+		Console.WriteLine();
+		Console.WriteLine("Parameterized Constructor");
+		
+		// Parameterized Constructor
+		Student student2 = new Student(2,"Girish",10,'F');
+		student2.ShowData();
+		
+		Console.WriteLine();
+		
+		Student student3 = new Student(3,"Human",5,'B');
+		student3.ShowData();
+		
+		Console.WriteLine();
+		
+		Student student4 = new Student(4,"Boy",3,'G');
+		student4.ShowData();
+
+		Console.WriteLine();
+
+		Console.WriteLine("Copy Constructor");
+		// Copy Constructor
+		Student student5 = new Student(student3);
+		student5.ShowData();
+	}
+}
---
+++ Student.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+	internal class Student
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public int Class {  get; set; }
+		public char Division { get; set; }
+		public static int TotalStudent {  get; set; }
+
+		// Static Constructor
+		static Student()
+		{
+			TotalStudent = 4;
+		}
+
+		// ParameterLess Constructor
+		public Student()
+		{
+			this.Id = 1;
+			this.Name = "Raj";
+			this.Class = 9;
+			this.Division = 'A';
+		}
+
+		// Parameterized Constructor
+		public Student(int id, string name, int cLass, char division)
+		{
+			this.Id= id;
+			this.Name = name;
+			this.Class= cLass;
+			this.Division = division;
+		}
+
+		// Copy Constructor
+		public Student(Student student)
+		{
+			this.Id = student.Id;
+			this.Name = student.Name;
+			this.Class = student.Class;
+			this.Division = student.Division;
+		}
+
+		public void ShowData()
+		{
+			Console.WriteLine("Student Id = " + this.Id);
+			Console.WriteLine("Student Name = "+ this.Name);
+			Console.WriteLine("Student Class = "+ this.Class);
+			Console.WriteLine("Student Division = "+ this.Division);
+		}
+	}
+}
